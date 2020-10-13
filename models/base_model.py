9c4f7f008054b566d159805a4953dc882a1f54a0
Viet Tran <vietnamtran@gmail.com>
2020-10-13 20:04:24 +0000

    Add to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,14 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
+
     def delete(self):
         """delete the current instance from the storage"""
         models.storage.delete(self)
