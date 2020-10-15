9db0f54dde890bc82b87b9a02c1b607d06178908
GunterPearson <1715@holbertonschool.com>
2020-10-15 15:55:08 -0500

    pushing front-end changes

--- web_flask/interests_list.py
+++ web_flask/interests_list.py
@@ -2,8 +2,9 @@
 """ Starts a Flask Web Application """
 from models import storage
 from models.interest import Interest
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def close_db(error):
@@ -16,10 +17,16 @@ def interests_list():
     """ displays a HTML page with a list of interests """
     interests = storage.all(Interest).values()
     interests = sorted(interests, key=lambda k: k.name)
+    test_list = []
+    i = 0
+    while i < 3:
+        test_list.append(interests[i])
+        i += 1
+    # print(test_list)
     ###CHANGE NAME OF HTML FILE AS NEEDED###
-    return render_template('interests_list.html', interests=interests)
+    return render_template("c.html", interests=test_list)
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
