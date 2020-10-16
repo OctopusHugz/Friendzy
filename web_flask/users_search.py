cf1cd6293cc1be69f01d7e5738966ea28cb80e0e
OctopusHugz <colsonscott53@gmail.com>
2020-10-16 23:03:49 +0000

    Modified and updated frontend users_search endpoint to take interest_id in query string. Updated html files with url_for command to handle all static files.

--- web_flask/users_search.py
+++ web_flask/users_search.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flask Web Application """
-import json
+from models.interest import Interest
 from flask.json import jsonify
 from models import storage
 from flask import Flask, render_template
@@ -16,12 +16,29 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/users_search', strict_slashes=False, methods=['POST', 'GET'])
-def users_search_list():
+@app.route('/interests_list', strict_slashes=False)
+def interests_list():
+    """ displays a HTML page with a list of interests """
+    interests = storage.all(Interest).values()
+    interests = sorted(interests, key=lambda k: k.id)
+    test_list = []
+    full_list = []
+    i = 0
+    while i < len(interests) - 1:
+        if i % 3 == 0 and i != 0:
+            full_list.append(test_list)
+            test_list = []
+        test_list.append(interests[i])
+        i += 1
+    return render_template("home.html", full_list=full_list)
+
+
+@app.route('/users_search/<interest_id>', strict_slashes=False, methods=['POST', 'GET'])
+def users_search_list(interest_id):
     """ displays a HTML page with a list of users """
-    # Interest_id is hardcoded below, needs to become dynamic
-    payload = '{"interests": [2]}'
+    payload = '{"interests": [' + interest_id + ']}'
     users = requests.post('http://127.0.0.1:5001/api/v1/users_search', data=payload, headers={'Content-Type': 'application/json'}).json()
+    # print(users)
     test_list = []
     full_list = []
     i = 0
