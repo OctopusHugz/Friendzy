a048710cf4c013192956079afbf0ffb6a5398636
Viet Tran <vietnamtran@gmail.com>
2020-10-20 21:06:18 +0000

    Add keypress event handler

--- web_flask/static/js/search.js
+++ web_flask/static/js/search.js
@@ -34,19 +34,25 @@ $(document).ready(function () {
   $('.autocomplete').autocomplete({
     source: interests
   });
-});
-$('#search_button').click(function (e) {
-  e.preventDefault();
-  const searchInterest = $('#search_text').val();
-  let index = 0;
-  interests.forEach(interest => {
-    if (interest === searchInterest) {
-      const interestID = index + 1;
-      const url = 'http://127.0.0.1:5000/users_search/' + interestID;
-      window.location.href = url;
-    }
-    index++;
-  });
+    $('#search_button').click(function (e) {
+        e.preventDefault();
+        const searchInterest = $('#search_text').val();
+        let index = 0;
+        interests.forEach(interest => {
+            if (interest === searchInterest) {
+                const interestID = index + 1;
+                const url = 'http://127.0.0.1:5000/users_search/' + interestID;
+                window.location.href = url;
+            }
+            index++;
+        });
+    });
+
+    $('#search_text').keyup(function (e) {
+        if (e.which == 13) {
+            $('#search_button').click();
+        }
+    });
 });
 // Get interest name when search button is clicked
 // Use interest name to get interest_id
