cd19ee4436aa70093d8f2f948701faf205853a46
Viet Tran <vietnamtran@gmail.com>
2020-10-21 00:39:41 +0000

    Made interests in user cards clickable links

--- web_flask/static/js/search.js
+++ web_flask/static/js/search.js
@@ -34,6 +34,7 @@ $(document).ready(function () {
   $('.autocomplete').autocomplete({
     source: interests
   });
+  // Click search button returns users based on interest in search bar
   $('#search_button').click(function (e) {
     e.preventDefault();
     const searchInterest = $('#search_text').val();
@@ -47,15 +48,31 @@ $(document).ready(function () {
       index++;
     });
   });
+  // Allow pressing enter to execute code above
   $('#search_text').keyup(function (e) {
     if (e.which === 13) {
       $('#search_button').click();
     }
   });
+  // Click random button to return a random interest
   $('#random_button').click(function (e) {
     e.preventDefault();
     const interestID = Math.floor(Math.random() * 30) + 1;
     const url = 'http://127.0.0.1:5000/users_search/' + interestID;
     window.location.href = url;
+  });
+  // Interests below user cards are clickable links
+  $("button.link").click(function (e) {
+    e.preventDefault();
+    const searchInterest = $(this).attr('id');
+    let index = 0;
+    interests.forEach(interest => {
+      if (interest === searchInterest) {
+        const interestID = index + 1;
+        const url = 'http://127.0.0.1:5000/users_search/' + interestID;
+        window.location.href = url;
+      }
+      index++;
+    });
   });
 });
