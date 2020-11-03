c36fd4e84c85b6bd7754d16724a67edba7c17af6
Viet Tran <vietnamtran@gmail.com>
2020-11-03 17:23:25 +0000

    Delete server_files directory

--- server_files/scripts/gunicorn_us.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Setup to run users_search application, used in service file
-gunicorn --workers=3 --error-logfile /tmp/users_search-error.log --access-logfile /tmp/users_search-access.log --bind 0.0.0.0:5000 web_flask.users_search:app
